5c5a8422a04d9a7c7c3ba167e58f70ef795e3134
Pieter Provoost <pieterprovoost@gmail.com>
2019-12-02 11:39:10 +0100

    ANZ fix

--- demo/anz_maps.R
+++ demo/anz_maps.R
@@ -15,16 +15,19 @@ stats <- df %>%
   group_by(longitude, latitude) %>%
   summarize(events = length(unique(eventName)), years = length(unique(eventYear)))
 range(stats$events)
-events_scale <- scale_radius(limits = c(1, 20), range = c(1.5, 8), breaks = c(1, 5, 10, 15, 20))
+scale_upper <- (ceiling(max(stats$events) / 5) * 5)
+events_scale <- scale_radius(
+  limits = c(1, scale_upper),
+  range = c(1.5, 8),
+  breaks = c(1, seq(5, scale_upper, by = 5))
+)
 
-color <- "#ff704d"
+color <- "#aaaaaa" #ff704d
 
 for (area in list_areas) {
-  
   path <- paste0("demo/output/events_", area$name, ".png")
   message(path)
   make_map(df, area = area, type = "events", scale = events_scale, line_color = "black", line_width = 0.75, color = color) +
     labs(title = "Events")
   ggsave(path, height = 8, width = 12, scale = 0.8)
-  
 }
